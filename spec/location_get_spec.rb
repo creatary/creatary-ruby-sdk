cb0a14eaa5f358323d0d55a0a840ebc84836914f
Lukasz Jastrzebski <lukasz.jastrzebski@gmail.com>
2012-01-24 14:14:14 -0800

    small problems with git usage

--- spec/location_get_spec.rb
+++ spec/location_get_spec.rb
@@ -1,28 +1,28 @@
 require 'spec_helper'
-require 'tam/api'
-require 'tam/user'
+require 'creatary/api'
+require 'creatary/user'
 require 'rack/test'
 require 'json'
 require 'webmock/rspec'
 
-describe TAM::API, "#getcoord" do
+describe Creatary::API, "#getcoord" do
   include WebMock::API
   include OAuthSettings
   
   it "sends location request to platform" do
     #given
-    TAM.expects(:consumer_key).twice().returns(ConsumerKey)
-    TAM.expects(:consumer_secret).twice().returns(ConsumerSecret)
-    TAM.expects(:site).returns(Site)
-    TAM.expects(:request_token_path).returns('/api/1/oauth/request_token')
-    TAM.expects(:access_token_path).returns('/api/1/oauth/access_token')
-    TAM.expects(:authorize_path).returns('/web/authorize')
-    TAM.expects(:oauth_scheme).returns(:query_string)
-    TAM.expects(:oauth_http_method).returns(:get)
-    http_stub = stub_request(:get, /.*\/api\/1\/location\/getcoord.*/).with { |request| assert_query(request.uri, OAuthParams) }.to_return(:body => load_fixture("tam_getcoord_success.json"))
+    Creatary.expects(:consumer_key).twice().returns(ConsumerKey)
+    Creatary.expects(:consumer_secret).twice().returns(ConsumerSecret)
+    Creatary.expects(:site).returns(Site)
+    Creatary.expects(:request_token_path).returns('/api/1/oauth/request_token')
+    Creatary.expects(:access_token_path).returns('/api/1/oauth/access_token')
+    Creatary.expects(:authorize_path).returns('/web/authorize')
+    Creatary.expects(:oauth_scheme).returns(:query_string)
+    Creatary.expects(:oauth_http_method).returns(:get)
+    http_stub = stub_request(:get, /.*\/api\/1\/location\/getcoord.*/).with { |request| assert_query(request.uri, OAuthParams) }.to_return(:body => load_fixture("getcoord_success.json"))
                   
     #when
-    body = TAM::API.getcoord(TAM::User.new(AccessToken, AccessSecret))
+    body = Creatary::API.getcoord(Creatary::User.new(AccessToken, AccessSecret))
     
     #then
     http_stub.should have_been_requested.times(1)
